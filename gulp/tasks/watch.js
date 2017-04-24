0c4ba46834e1e68c8c7591b1f6e1c120f17db093
Jeff <jeffcombs@wks-jefcombs.local>
2017-04-24 17:09:09 -0500

    Completed the testimonials section

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,20 +4,21 @@ browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
 
-    browserSync.init({
-      notify: false,
-      server: {
-        baseDir: "app"
-      }
-    });
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
 
-    watch('./app/index.html', function() {
-        browserSync.reload();
-    });
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
 
-    watch('./app/assets/styles/**/*.css', function() {
-        gulp.start('cssInject');
-    });
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
 });
 
 gulp.task('cssInject', ['styles'], function() {
